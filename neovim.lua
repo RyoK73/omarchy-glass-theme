e81bfe07fb0c81aa24e05367c11e0ef7708435c3
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-05-31 21:27:28 +0900

    feat:カラー設定をアップデート

--- neovim.lua
+++ neovim.lua
@@ -1,13 +1,14 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-nvim",
-    },
-  },
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        config = function()
+            require("onedark").setup({
+                style = "warm",
+                toggle_style_key = "<leader>ts",
+                toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+            })
+            require("onedark").load()
+        end,
+    }
 }
